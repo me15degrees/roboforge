83d1af0aac7010a2af827c328e6689e2c20029fb
me15degrees <me15degrees@gmail.com>
2024-06-11 14:54:31 -0300

    código do sensor LDR com porcentagem

--- estudos/estudo_ldr.cpp
+++ estudos/estudo_ldr.cpp
@@ -6,12 +6,12 @@ class LDR {
       resolution = adcResolution;
       voltage = refVoltage;
       threshold = (1 << resolution) / 2; // limite para distinguir entre preto e branco
-      gndPin = 0; // configura o pino GND (padrão)
+      gndPin = 0; //configura o pino GND (padrão)
     }
 
     // método para ler o valor do sensor e converter para qualitativo
     int readSensor() {
-      int sensorValue = analogRead(sensorPin); // lê o valor do pino analógico (0 a 1023)
+      sensorValue = analogRead(sensorPin); // lê o valor do pino analógico
       if (sensorValue < threshold) {
         return 0; // cor preta
       } else {
@@ -19,23 +19,32 @@ class LDR {
       }
     }
 
+    // método para retornar a leitura do sensor em porcentagem
+    float percentageSensor() {
+      sensorValue = analogRead(sensorPin); // lê o valor do pino analógico
+      return (sensorValue / float(1 << resolution)) * 100; // retorna o valor em porcentagem
+    }
+
   private:
     int sensorPin; // pino analógico do sensor
     int gndPin; // pino GND (padrão)
     int resolution; // resolução ADC (bits)
-    int voltage; // tensão de referência
+    float voltage; // tensão de referência
     int threshold; // limite para distinguir entre preto e branco
+    int sensorValue; // valor lido do sensor
 };
 
 // instancia o sensor LDR esquerdo no pino A0
-LDR sensor_esquerdo(A0, 10, 5.0);
+LDR sensorTeste(A0, 10, 5.0);
 
 void setup() {
   Serial.begin(9600); // inicia a comunicação serial a 9600 bps - ARDUINO UNO (?)
 }
 
 void loop() {
-  int result = sensor_esquerdo.readSensor(); // lê o valor qualitativo do sensor
-  Serial.println(result); // imprime o resultado (0 ou 1) no monitor serial
+  int bin = sensorTeste.readSensor(); // lê o valor qualitativo do sensor
+  float percentage = sensorTeste.percentageSensor(); // lê a porcentagem do sensor
+  Serial.println(bin); // imprime o resultado (0 ou 1) no monitor serial
+  Serial.println(percentage); // imprime a porcentagem no monitor serial
   delay(300); // espera 0,3 segundos antes de ler novamente
 }
