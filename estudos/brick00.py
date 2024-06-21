f4c6156d801c5189576974c782180e937a42c5ab
me15degrees <me15degrees@gmail.com>
2024-06-21 15:31:00 -0300

    funções de movimentação corrigidas

--- estudos/brick00.py
+++ estudos/brick00.py
@@ -10,103 +10,117 @@ from time import sleep
 
 ev3 = EV3Brick()
 
-# achei num repositório e quis testar aqui
-ev3.speaker.set_volume(volume=100, which="_all_")
-ev3.speaker.set_speech_options(language="pt-pt", voice="whisperf")
-ev3.speaker.say("RO BO FOR GE")
-ev3.speaker.beep(frequency=1000, duration=100)
+ev3.speaker.beep()
 
 class Sumo:
-    def __init__(self, velocidade, wheel_diameter, wheel_distance):
+    def _init_(self, velocidade, wheel_diameter, wheel_distance):
         self.velocidade = velocidade
+        self.wheel_diameter = wheel_diameter
         self.wheel_lenght = wheel_diameter * 3.1415
         self.wheel_distance = wheel_distance
-        self.r_motor = Motor(Port.B) # esses motores provavelmente não vão mudar de lugar
-        self.l_motor = Motor(Port.C)
-        self.color_front = ColorSensor(Port.S1) #sensor de cor na frente 
-        self. ultra_front = UltrasonicSensor(Port.S2) #sensor ultrassonico na frente  
-
-    def walk(self, speed=100):
+        self.r_motor = Motor(Port.A) # esses motores provavelmente não vão mudar de lugar
+        self.l_motor = Motor(Port.B)
+        self.r1_motor = Motor(Port.D) # esses motores provavelmente não vão mudar de lugar
+        self.l1_motor = Motor(Port.C)
+        self.ultra_sens1 = UltrasonicSensor(Port.S1) #sensor de cor na frente
+        self.ultra_sens2 = UltrasonicSensor(Port.S2) #sensor de cor na frente 
+     
+    def walk(self, speed=300):
         self.r_motor.run(speed)
         self.l_motor.run(speed)
+        self.r1_motor.run(-speed)
+        self.l1_motor.run(-speed)
 
-    def attack(self, speed=250): # ataque com mais velocidade
-        while self.color_front.color() != color.WHITE:
+    def attack(self, speed=450): # ataque com mais velocidade
             self.r_motor.run(speed)
             self.l_motor.run(speed)
+            self.r1_motor.run(-speed)
+            self.l1_motor.run(-speed)
 
     def hold_motors(self):
         self.r_motor.hold()
         self.l_motor.hold()
-    
+        self.r1_motor.hold()
+        self.l1_motor.hold()
+
     def brake_motors(self):
         self.r_motor.brake()
         self.l_motor.brake()
+        self.r1_motor.brake()
+        self.l1_motor.brake()
 
     def right(self, angle, speed):
+        print("\n----------------DIREITA----------------")
         self.l_motor.reset_angle(0)
         self.r_motor.reset_angle(0)
+        self.r1_motor.reset_angle(0)
+        self.l1_motor.reset_angle(0)
         media_motor = 0
-        graus_motor = angle * (self.wheel_distance / self.wheel.diameter)
+        graus_motor = angle * (self.wheel_distance / self.wheel_diameter)
+    
         while media_motor < graus_motor:
             self.l_motor.run(speed)
             self.r_motor.run(-speed)
-            media_motor = (self.l_motor.angle() - self.r_motor.angle()) / 2
-        print(f"\nO motor girou para direita {media_motor} graus")
-        self.l_motor.hold()
+            self.r1_motor.run(speed)
+            self.l1_motor.run(-speed)
+            print(media_motor)
+            media_motor = (self.l_motor.angle() - self.r_motor.angle()) / 2 # entender melhor o comportamento do media motor
         self.r_motor.hold()
+        self.l_motor.hold()
+        self.r1_motor.hold()
+        self.l1_motor.hold()
 
     def left(self, angle, speed=100):
+        print("\n----------------ESQUERDA----------------")
         self.l_motor.reset_angle(0)
         self.r_motor.reset_angle(0)
+        self.r1_motor.reset_angle(0)
+        self.l1_motor.reset_angle(0)
+       
         media_motor = 0
-        graus_motor = angle * (self.wheel_distance / self.wheel.diameter)
+        graus_motor = angle * (self.wheel_distance / self.wheel_diameter) 
+        
         while media_motor < graus_motor: # para de girar até identificar que girou o ângulo ideal
             self.l_motor.run(-speed)
             self.r_motor.run(speed)
-            media_motor = (self.l_motor.angle() - self.r_motor.angle()) / 2
-        print(f"\nO motor girou para a esqueerda {media_motor} graus")
-        self.l_motor.hold()
+            self.r1_motor.run(-speed)
+            self.l1_motor.run(speed)
+            print(media_motor)
+            media_motor = -1*((self.l_motor.angle()) - self.r_motor.angle()) / 2 # entender melhor o comportamento do media motor
         self.r_motor.hold()
+        self.l_motor.hold()
+        self.r1_motor.hold()
+        self.l1_motor.hold()
 
-robo_sumo = Sumo(100,5.5,11.5) # meu objeto <3 
+robo_sumo = Sumo(100,4.5,11.8) 
 
-def search (speed=100):
-    while True:
-        robo_sumo.right(120)
-        robo_sumo.left(120)
-        robo_sumo.right(30)
-        robo_sumo.walk(200)
-        sleep(4)
-        robo_sumo.left(180)
-        robo_sumo.right(120)
-        robo_sumo.left(120)
+def search(speed=200):
+    angle=120
+    #ponto inicial
+    robo_sumo.right(angle, speed)
+    robo_sumo.hold_motors()
+    robo_sumo.left(2*angle, speed)
+    robo_sumo.hold_motors()
+    robo_sumo.right(95, speed) # fazer ele retornar do ponto inicial
+    sleep(0.5) 
         
 def main():
-    
-    ENEMY_DISTANCE = 800 
+    ENEMY_DISTANCE = 100
     flag = 0
     
-    while not flag: # para garantir que o botão vai ser apertado
+    while not flag:  # espera pelo botão central
         for button in ev3.buttons.pressed():
             print(button)
             if button == Button.CENTER:
                 flag = 1
-    sleep(5)
-    
-    while True:
+
+    distance = (robo_sumo.ultra_sens1.distance() + robo_sumo.ultra_sens2.distance()) / 2 # media dos dois sensores ultrassônicos
+    while distance >= ENEMY_DISTANCE:
+        search()
+        sleep(3)
+        distance = (robo_sumo.ultra_sens1.distance() + robo_sumo.ultra_sens2.distance()) / 2
         robo_sumo.walk()
-        robo_sumo.hold_motors()
-        robo_sumo.brake_motors()
-        robo_sumo.attack()
-        robo_sumo.right(90)
-        robo_sumo.left(90)
-        sleep(5)
-        distance = robo_sumo.ultra_front.distance()
-        
-        if distance <= ENEMY_DISTANCE:
-            robo_sumo.attack()
-        
-        else:
-            robo_sumo.search()
+        sleep(2)
+    robo_sumo.attack()
+
 main()
