3ae7cfc6cd3c5222e6fd174a092467c390ad7da6
me15degrees <me15degrees@gmail.com>
2024-06-25 20:30:20 -0300

    arquivo para entender o funcionamento do seguidor de linha e o teste de uma inicialização do brick

---
+++ estudos/testes/som.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+ev3 = EV3Brick()
+
+# achei num repositório e quis testar aqui
+ev3.speaker.set_volume(volume=100, which="_all_")
+ev3.speaker.set_speech_options(language="pt-pt", voice="whisperf")
+ev3.speaker.say("RO BO FOR GE")
+ev3.speaker.beep(frequency=1000, duration=100)
+
